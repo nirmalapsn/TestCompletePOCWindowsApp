292086841d827fdd9a141fe91306650dbb31b3da
nirmalapsn <srinaganirmala@gmail.com>
2024-03-13 17:12:21 -0400

    LogUtils

--- POCProjectSuite/CalcProject/Script/CalculatorTests.js
+++ POCProjectSuite/CalcProject/Script/CalculatorTests.js
@@ -1,18 +1,27 @@
 ﻿//USEUNIT JsonUtils
 //USEUNIT CalculatorActions
+//USEUNIT LogUtils
 
 var stepNo = 1;
 var jsonFilePath;
 
 function testAddNumbers(){
+  setLogFolderAttributes("Step: " + stepNo++ + ": " + "Read JSON File");
   jsonFilePath = ProjectSuite.Path + "TestData\\CalculatorTestData.Json";
   var jsonObjParent = readJsonFile(jsonFilePath);
   var testData = jsonObjParent.Add;
+  addLogsWithAttr("Checkpoint", "Test Data for Addition: " + testData, pmNormal, true, false, clWhite, clGreen);
+  Log.PopLogFolder();
+  
+  setLogFolderAttributes("Step: " + stepNo++ + ": " + "Perform Addition on Numbers");
   var sum = addNumbers(testData);
   if(sum!=null && sum!=undefined)
-    Log.Checkpoint("Sum: " + sum);
+    addLogsWithAttr("Checkpoint", "Sum: " + sum, pmHigher, true, false, clWhite, clGreen);
+    //Log.Checkpoint("Sum: " + sum);
   else
-    Log.Error("Sum Calculation failed");
+    //Log.Error("Sum Calculation failed");
+    addLogsWithAttr("Error", "Sum Calculation failed", pmHigher, true, false, clWhite, clRed);
+  Log.PopLogFolder();
 }
   
 
--- POCProjectSuite/CalcProject/Script/LogUtils.js
+++ POCProjectSuite/CalcProject/Script/LogUtils.js
@@ -1,3 +1,46 @@
-﻿function setLogFolderAttributes(){
+﻿function setLogFolderAttributes(logFolderName, priority, boolBold, boolItalic, backColor, fontColor){
+  var logFolderAttr = Log.CreateNewAttributes();
+  if(!priority)
+    priority = pmNormal;
+  if(!boolBold)
+    logFolderAttr.Bold = true;
+  if(!boolItalic)
+    logFolderAttr.Italic = false;
+  if(!backColor)
+    logFolderAttr.BackColor = clWhite;
+  if(!fontColor)
+    logFolderAttr.FontColor = clNavy;
+    
+  var folderId = Log.AppendFolder(logFolderName, "", priority, logFolderAttr);
+  return folderId;
+}
+
+function addLogsWithAttr(logMessageType, logMessage, priority, boolBold, boolItalic, backColor, fontColor){
+  let logAttr = Log.CreateNewAttributes();
+  if(!priority)
+    priority = pmNormal;
+  if(!boolBold)
+    boolBold = false;
+  if(!boolItalic)
+    boolItalic = false;
+  if(!backColor)
+    backColor = clWhite;
+  if(!fontColor)
+    fontColor = clBlack;
+    
+  logAttr.Bold = boolBold;
+  logAttr.Italic = boolItalic;
+  logAttr.BackColor = backColor;
+  logAttr.FontColor = fontColor;
   
+  if(logMessageType == "Checkpoint")
+    Log.Checkpoint(logMessage, "", priority, logAttr);
+  if(logMessageType == "Event")
+    Log.Event(logMessage, "", priority, logAttr);
+  if(logMessageType == "Error")
+    Log.Error(logMessage, "", priority, logAttr);
+  if(logMessageType == "Warning")
+    Log.Warning(logMessage, "", priority, logAttr);
+  if(logMessageType == "Message")
+    Log.Message(logMessage, "", priority, logAttr);
 }
